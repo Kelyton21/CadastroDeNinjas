3a58ffe4b02d03870da6fbfb3e7de57bfbfb8a53
@Kelyton21 <kelyton2004@gmail.com>
2025-09-11 16:04:46 -0300

    feat: Criado os repositories das entitades

--- src/main/java/dev/developer/CadastroDeNinja/Repository/MissaoRepository.java
+++ src/main/java/dev/developer/CadastroDeNinja/Repository/MissaoRepository.java
@@ -1,2 +1,8 @@
-package dev.developer.CadastroDeNinja.Repository;public class MissaoRepository {
+package dev.developer.CadastroDeNinja.Repository;
+
+import dev.developer.CadastroDeNinja.Model.Missoes.MissaoModel;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel,Long> {
 }
--- src/main/java/dev/developer/CadastroDeNinja/Repository/NinjaRepository.java
+++ src/main/java/dev/developer/CadastroDeNinja/Repository/NinjaRepository.java
@@ -1,2 +1,7 @@
-package dev.developer.CadastroDeNinja.Repository;public class NinjaRepository {
+package dev.developer.CadastroDeNinja.Repository;
+
+import dev.developer.CadastroDeNinja.Model.Ninjas.NinjaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
 }
