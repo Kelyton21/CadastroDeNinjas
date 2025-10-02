22044faed85219dcc63c444966e28c68be35e773
@Kelyton21 <kelyton2004@gmail.com>
2025-10-02 00:50:35 -0300

    feat: corrigi no porm.xml o problema do getter e setter do lombok

--- src/main/java/dev/developer/CadastroDeNinja/Model/Ninjas/NinjaModel.java
+++ src/main/java/dev/developer/CadastroDeNinja/Model/Ninjas/NinjaModel.java
@@ -4,7 +4,8 @@ import dev.developer.CadastroDeNinja.Model.Missoes.MissaoModel;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Builder
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_ninja")
 @EqualsAndHashCode(of = "id")
@@ -31,40 +32,5 @@ public class NinjaModel {
     public NinjaModel() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public MissaoModel getMissao() {
-        return missao;
-    }
-
-    public void setMissao(MissaoModel missao) {
-        this.missao = missao;
-    }
 }
 
